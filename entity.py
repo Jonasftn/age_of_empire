e06b18a830e3375af9d486c00420351d783d39a4
jmichel <>
2025-01-26 20:38:54 +0100

    Initial commit local

---
+++ entity.py
@@ -0,0 +1,67 @@
+from math import sqrt
+import math
+import pygame
+
+from TileMap import *
+from constants import *
+from Coordinates import *
+import random
+import time
+from Global_image_load import *
+from numpy.random import poisson
+
+class Entity:
+    def __init__(self, gameObj, entityType, position):
+
+        self.gameObj = gameObj
+        """
+        self.tile_grass = tile_grass
+        self.map_data = map_data  # Dictionnaire global à modifier
+        self.compteurs_joueurs = compteurs_joueurs
+        self.unit_list = None
+        self.current_unit_index = 0
+
+        self.coordinates = Coordinates()
+        self.id = None  # Identifiant unique pour l'unité
+
+        self.position = None
+        self.target_position = None
+        self.moving = False
+        self.deplacement_termine = False
+        self.move_start_time = 0
+        self.move_duration = 0
+        self.frame_index = 0
+        self.direction_index = 0
+        self.last_time = pygame.time.get_ticks()
+        self.destination = None
+        self.moving_unit=None
+
+        self.start_time_offset = 0
+        self.attacks_in_progress = []
+        """
+        self.position = position
+        self.entityType = entityType
+        
+
+
+    def set_final_position(x, y):
+        self.positionFinal = (x, y) 
+
+    def is_moving():
+        return self.positionFinal <> self.position 
+
+class Person(Entity):
+    def __init__(self):
+        self.healthPoint = constants.units_dict[entityType]['hp']
+        self.image = constants.units_dict[entityType]['image']
+
+class Ressource(Entity):
+    def __init__(self):
+        self.quantity = constants.ressources_dict[entityType]['quantite']
+        self.image = constants.ressources_dict[entityType]['image']
+
+class Building(Entity):
+    def __init__(self):
+        self.healthPoint = constants.builds_dict[entityType]['hp']
+        self.image = constants.builds_dict[entityType]['image']
+        
